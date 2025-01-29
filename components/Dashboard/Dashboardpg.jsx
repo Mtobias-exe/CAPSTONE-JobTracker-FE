f96002f639a26484e38a73e54b19e1749cc12b55
Mtobias-exe <maryfrance.tobias@gmail.com>
2025-01-29 13:38:41 -0500

    moved form data to add page

--- components/Dashboard/Dashboardpg.jsx
+++ components/Dashboard/Dashboardpg.jsx
@@ -1,66 +1,9 @@
-import React, { useState } from 'react'
 import './Dashboardpg.css'
+import React from 'react'
 
-
-
-const Dashboardpg = () =>{
-  const [formData, setFormData] = useState({
-    companyName: '',
-    jobTitle: '',
-    date: '',
-    status: '',
-    notes: ''
-});
-
+function Dashboardpg() {
   return (
-    <section>
-    <form>
-     
-    <label htmlFor="companyName">Company Name</label>
-      <input
-      type='text'
-      id='companyName'
-      name='companyName'
-      value={formData.companyName}
-      />
-      
-      <label htmlFor="jobTitle">Job Title</label>
-      <input
-       type='text'
-       id='jobTitle'
-       name='jobTitle'
-       value={formData.jobTitle}
-      />
-      
-      <label htmlFor="date">Date</label>
-      <input
-       type='date'
-       id='date'
-       name='date'
-       value={formData.date}
-      />
-
-      <label htmlFor="status">Status</label>
-      <select
-       id='status'
-       name='date'
-       value={formData.status}
-      >
-      <option value="pending">Pending</option>
-      <option value="rejected">Rejected</option>
-      <option value="accepted">Accepted</option>
-    </select>
-
-      <label htmlFor="note">Notes</label>
-      <input
-       type='text'
-       id='note'
-       name='note'
-       value={formData.notes}
-      />
-
-    </form>
-    </section>
+    <div>Dashboardpg</div>
   )
 }
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import LandingPg from '../components/LandingPage/LandingPg'
 import Dashboardpg from '../components/Dashboard/Dashboardpg'
+import AddPg from '../components/AddPage/AddPg'
 
 function App() {
 
   return (
     <>
       <LandingPg/>
-
+     
     
     </>
   )
