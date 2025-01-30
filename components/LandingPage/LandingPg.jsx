710ae704a59fc23dde7758e96291ae9176dfec5f
Mtobias-exe <maryfrance.tobias@gmail.com>
2025-01-30 00:21:54 -0500

    working on landing page styling

--- components/LandingPage/LandingPg.jsx
+++ components/LandingPage/LandingPg.jsx
@@ -12,7 +12,7 @@ function LandingPg() {
                 <div className='column-left'>
                     <h1>Job Tracker</h1>
                     <p>Feeling Overwhelmed by Job Hunting? Let Job Tracker Simplify Your Search</p>
-                    <button>Get Started</button>
+                    <button className='CTA'>Let's Get Started</button>
                 </div>
                 <div className='column-right'>
                     <img src='/landingpage.png' alt='working-illustration' className='hero-img'/>    
